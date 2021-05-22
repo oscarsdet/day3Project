37023b1ee0d7fb778be8607bb358ea242fdcaabd
Deum <oscar.sdet@gmail.com>
2021-05-22 11:21:55 -0400

    added additional hello enhancement

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // enhancing hello
+        System.out.println("I Love githuh");
     }
 }
