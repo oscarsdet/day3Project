30e0b65b217f523f804c004f0d7a64d93394a0ee
Deum <oscar.sdet@gmail.com>
2021-05-22 11:17:54 -0400

    added hello world

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello world ");
-        System.out.println("don't Blink please so we dont get lost ");
-    }
 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
